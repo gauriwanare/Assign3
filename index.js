7eb26350d4072a52ff9d3bad7f2eac53fae9de23
gauriwanare <gauriwanare21@gmail.com>
2023-07-23 17:22:08 +0530

    Added Priorities, Working on Sorting

--- index.js
+++ index.js
@@ -14,11 +14,13 @@ window.addEventListener('load', () => {
 	newTodoForm.addEventListener('submit', e => {
 		e.preventDefault();
 		const dueDate = document.getElementById('due-date').value;
+		const priority = document.getElementById('priority').value;
 		const todo = {
 			content: e.target.elements.content.value,
 			category: e.target.elements.category.value,
 			done: false,
 			dueDate: dueDate,
+			priority: priority,
 			createdAt: new Date().getTime()
 		}
 
@@ -30,6 +32,8 @@ window.addEventListener('load', () => {
 		e.target.reset();
 
 		DisplayTodos()
+		// renderTodos('dueDate') // Sort todos by due date
+		// renderTodos('priority'); // Sort todos by priority
 	})
 
 	DisplayTodos()
@@ -65,7 +69,8 @@ function DisplayTodos () {
 		deleteButton.classList.add('delete');
 
 		content.innerHTML = `<input type="text" value="${todo.content}" readonly>
-		<p>Due Date: ${todo.dueDate}</p>`;
+		<p>Due Date: ${todo.dueDate}</p>
+		<p>Priority: ${todo.priority}</p>`;
 		edit.innerHTML = 'Edit';
 		deleteButton.innerHTML = 'Delete';
 
@@ -118,3 +123,114 @@ function DisplayTodos () {
 
 	})
 }
+
+
+
+function renderTodos () {
+	const todoList = document.querySelector('#todo-list');
+	todoList.innerHTML = "";
+	let sortedTodos = todos;
+
+	if (sortOrder === 'dueDate') {
+		sortedTodos = todos.slice().sort((a, b) => {
+		if (!a.dueDate) return 1;
+		if (!b.dueDate) return -1;
+		return new Date(a.dueDate) - new Date(b.dueDate);
+		});
+	} else if (sortOrder === 'priority') {
+		sortedTodos = todos.slice().sort((a, b) => {
+		if (!a.priority) return 1;
+		if (!b.priority) return -1;
+		if (a.priority === b.priority) return 0;
+		if (a.priority === 'High') return -1;
+		if (b.priority === 'High') return 1;
+		if (a.priority === 'Medium') return -1;
+		if (b.priority === 'Medium') return 1;
+		return 0;
+		});
+	}
+
+	sortedTodos.forEach(todo => {
+		const todoItem = document.createElement('div');
+		todoItem.classList.add('todo-item');
+
+		const label = document.createElement('label');
+		const input = document.createElement('input');
+		const span = document.createElement('span');
+		const content = document.createElement('div');
+		const actions = document.createElement('div');
+		const edit = document.createElement('button');
+		const deleteButton = document.createElement('button');
+
+		input.type = 'checkbox';
+		input.checked = todo.done;
+		span.classList.add('bubble');
+		if (todo.category == 'personal') {
+			span.classList.add('personal');
+		} else {
+			span.classList.add('Professional');
+		}
+		content.classList.add('todo-content');
+		actions.classList.add('actions');
+		edit.classList.add('edit');
+		deleteButton.classList.add('delete');
+
+		content.innerHTML = `<input type="text" value="${todo.content}" readonly>
+		<p>Due Date: ${todo.dueDate}</p>
+		<p>Priority: ${todo.priority}</p>`;
+		edit.innerHTML = 'Edit';
+		deleteButton.innerHTML = 'Delete';
+
+		label.appendChild(input);
+		label.appendChild(span);
+		actions.appendChild(edit);
+		actions.appendChild(deleteButton);
+		todoItem.appendChild(label);
+		todoItem.appendChild(content);
+		todoItem.appendChild(actions);
+
+		todoList.appendChild(todoItem);
+
+		if (todo.done) {
+			todoItem.classList.add('done');
+		}
+		
+		input.addEventListener('change', (e) => {
+			todo.done = e.target.checked;
+			localStorage.setItem('todos', JSON.stringify(todos));
+
+			if (todo.done) {
+				todoItem.classList.add('done');
+			} else {
+				todoItem.classList.remove('done');
+			}
+
+			DisplayTodos()
+
+		})
+
+		edit.addEventListener('click', (e) => {
+			const input = content.querySelector('input');
+			input.removeAttribute('readonly');
+			input.focus();
+			input.addEventListener('blur', (e) => {
+				input.setAttribute('readonly', true);
+				todo.content = e.target.value;
+				localStorage.setItem('todos', JSON.stringify(todos));
+				DisplayTodos()
+
+			})
+		})
+
+		deleteButton.addEventListener('click', (e) => {
+			todos = todos.filter(t => t != todo);
+			localStorage.setItem('todos', JSON.stringify(todos));
+			DisplayTodos()
+		})
+
+	})
+}
+
+
+
+
