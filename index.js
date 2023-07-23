6b7a0703d72358a7e50a2203b0eb3523fa6fa2ef
gauriwanare <gauriwanare21@gmail.com>
2023-07-23 19:39:30 +0530

    Added Filter Options

--- index.js
+++ index.js
@@ -39,116 +39,28 @@ window.addEventListener('load', () => {
 	DisplayTodos()
 })
 
-function DisplayTodos () {
+function DisplayTodos (filterCriteria) {
 	const todoList = document.querySelector('#todo-list');
 	todoList.innerHTML = "";
-	const myTodo = [...todos].reverse();
-	myTodo.forEach(todo => {
-		const todoItem = document.createElement('div');
-		todoItem.classList.add('todo-item');
+	const sortedTodos = [...todos].reverse();
 
-		const label = document.createElement('label');
-		const input = document.createElement('input');
-		const span = document.createElement('span');
-		const content = document.createElement('div');
-		const actions = document.createElement('div');
-		const edit = document.createElement('button');
-		const deleteButton = document.createElement('button');
-
-		input.type = 'checkbox';
-		input.checked = todo.done;
-		span.classList.add('bubble');
-		if (todo.category == 'personal') {
-			span.classList.add('personal');
-		} else {
-			span.classList.add('Professional');
-		}
-		content.classList.add('todo-content');
-		actions.classList.add('actions');
-		edit.classList.add('edit');
-		deleteButton.classList.add('delete');
-
-		content.innerHTML = `<input type="text" value="${todo.content}" readonly>
-		<p>Due Date: ${todo.dueDate}</p>
-		<p>Priority: ${todo.priority}</p>`;
-		edit.innerHTML = 'Edit';
-		deleteButton.innerHTML = 'Delete';
-
-		label.appendChild(input);
-		label.appendChild(span);
-		actions.appendChild(edit);
-		actions.appendChild(deleteButton);
-		todoItem.appendChild(label);
-		todoItem.appendChild(content);
-		todoItem.appendChild(actions);
-
-		todoList.appendChild(todoItem);
-
-		if (todo.done) {
-			todoItem.classList.add('done');
-		}
-		
-		input.addEventListener('change', (e) => {
-			todo.done = e.target.checked;
-			localStorage.setItem('todos', JSON.stringify(todos));
-
-			if (todo.done) {
-				todoItem.classList.add('done');
-			} else {
-				todoItem.classList.remove('done');
-			}
-
-			DisplayTodos()
-
-		})
-
-		edit.addEventListener('click', (e) => {
-			const input = content.querySelector('input');
-			input.removeAttribute('readonly');
-			input.focus();
-			input.addEventListener('blur', (e) => {
-				input.setAttribute('readonly', true);
-				todo.content = e.target.value;
-				localStorage.setItem('todos', JSON.stringify(todos));
-				DisplayTodos()
-
-			})
-		})
-
-		deleteButton.addEventListener('click', (e) => {
-			todos = todos.filter(t => t != todo);
-			localStorage.setItem('todos', JSON.stringify(todos));
-			DisplayTodos()
-		})
-
-	})
-}
-
-
-
-function renderTodos () {
-	const todoList = document.querySelector('#todo-list');
-	todoList.innerHTML = "";
-	let sortedTodos = todos;
-
-	if (sortOrder === 'dueDate') {
-		sortedTodos = todos.slice().sort((a, b) => {
-		if (!a.dueDate) return 1;
-		if (!b.dueDate) return -1;
-		return new Date(a.dueDate) - new Date(b.dueDate);
-		});
-	} else if (sortOrder === 'priority') {
-		sortedTodos = todos.slice().sort((a, b) => {
-		if (!a.priority) return 1;
-		if (!b.priority) return -1;
-		if (a.priority === b.priority) return 0;
-		if (a.priority === 'High') return -1;
-		if (b.priority === 'High') return 1;
-		if (a.priority === 'Medium') return -1;
-		if (b.priority === 'Medium') return 1;
-		return 0;
-		});
+	switch (filterCriteria) {
+        case 'dueDate':
+			sortedTodos.sort((a, b) => {
+                // Handling ToDos with no due date
+                if (!a.dueDate && !b.dueDate) return 0;
+                if (!a.dueDate) return 1;
+                if (!b.dueDate) return -1;
+                return new Date(a.dueDate) - new Date(b.dueDate);
+            });
+            break;
+        case 'priority':
+            const priorityMap = { 'High': 3, 'Medium': 2, 'Low': 1 };
+            sortedTodos.sort((a, b) => priorityMap[b.priority] - priorityMap[a.priority]);
+            break;
 	}
+
+
 
 	sortedTodos.forEach(todo => {
 		const todoItem = document.createElement('div');
@@ -231,6 +143,19 @@ function renderTodos () {
 	})
 }
 
+
+function handleFilterChange() {
+    const sortSelect = document.getElementById('sort');
+    const selectedCriteria = sortSelect.value;
+    DisplayTodos(selectedCriteria);
+}
+
+
+const sortSelect = document.getElementById('sort');
+sortSelect.addEventListener('change', handleFilterChange);
+
+
+DisplayTodos();
 
 
 
